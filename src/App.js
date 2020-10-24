3b8b867cb36914bd75c04ca0c36bb66bd99313e7
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-10-24 17:27:22 +0100

    Row component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD!</h1>
+      <Row title="NETFLIX ORIGINALS"/>
+      <Row title="Trending Now"/>
     </div>
   );
 }
