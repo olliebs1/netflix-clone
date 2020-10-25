3353524985ba6cfa8de5dea80c5e0235b74528b0
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-10-25 16:35:44 +0000

    async function created and console logging object returned from api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Row from './Row';
 import './App.css';
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD!</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
--- src/api_key.js
+++ src/api_key.js
@@ -1,1 +1,3 @@
 const APIKEY = 'fa20afa2dd6219e0c7b5516d98c1d588';
+
+export default APIKEY;
