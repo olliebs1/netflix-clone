f191a70db58644cf4bb4b3805b79a16af444d311
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-10-26 15:17:48 +0000

    map of movies now returning images on front end

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'; 
 import axios from './axios';
 
+const baseImgUrl = 'https://image.tmdb.org/t/p/original/';
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -8,15 +9,23 @@ function Row({title, fetchUrl}) {
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request)
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.table(movies);
 
     return (
-        <div> 
-           <h2>{title}</h2> 
+        <div className='row'> 
+           <h2>{title}</h2>
+
+           <div className='row_posters'>
+               {movies.map(movie => {
+                   return <img src={`${baseImgUrl}${movie.poster_path}`} alt={movie.name}/>
+               })}
+           </div> 
         </div>
     )
 }
