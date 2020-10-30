10edb6620efd47b1c7d51679d17cb5e999c87678
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-10-30 17:18:43 +0000

    event listener added to scrollY to modify navbar on scroll - css for posters edited

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './Nav.css';
 import logo from './netflix.png';
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && "nav_black"}`}>
            <img className='nav_logo' src={logo} alt='Netflix Logo'/> 
-           <img className='nav_avatar' src='' alt='Netflix Logo'/>
+           <img className='nav_avatar' src='https://i.redd.it/74kqeuxol1k41.png' alt='Netflix Logo'/>
         </div>
     )
 }
